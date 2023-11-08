529a09a66ef2ab4e938540aef69f9f03e93bdbd4
Soumya Sutar <soumsutar9@gmail.com>
2023-11-08 16:53:12 +0530

    Assignment_4

---
+++ Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment_4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter a number: ");
+            int number = int.Parse(Console.ReadLine());
+
+            if (number > 0)
+            {
+                Console.WriteLine("The number is positive.");
+            }
+            else if (number < 0)
+            {
+                Console.WriteLine("The number is negative.");
+            }
+            else
+            {
+                Console.WriteLine("The number is zero.");
+            }
+
+            string evenOrOdd = (number % 2 == 0) ? "even" : "odd";
+            Console.WriteLine($"The number is {evenOrOdd}.");
+        }
+    }
+}
+            
+
+
+
